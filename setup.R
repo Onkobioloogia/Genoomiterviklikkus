c21e088cb619a84d5a72974515f9f5c20da154b5
Taavi Päll <tapa741@gmail.com>
2017-11-23 18:43:31 +0200

    update

--- setup.R
+++ setup.R
@@ -1,9 +1,4 @@
-library(slidify)
-library(slidifyLibraries)
-# setwd("~/Dropbox/Onkobioloogia/")
-# author("Genoomiterviklikkus")
 
-# setwd("~/Dropbox/Onkobioloogia/Genoomiterviklikkus/")
-slidify('index.Rmd')
+slidify::slidify('index.Rmd', )
 browseURL("index.html")
 # publish("Genoomiterviklikkus", "tpall")
